e9e83fd4360147a456dbf7a0476ae1d81d5cdc1c
hungle546 <56492292+hungle546@users.noreply.github.com>
2022-02-16 20:11:32 -0800

    added both level and main menu UI screen

--- pyqt5/level_selector.py
+++ pyqt5/level_selector.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'start_screen.ui'
+# Form implementation generated from reading ui file 'level_selector.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
@@ -14,14 +14,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 596)
-        MainWindow.setFocusPolicy(QtCore.Qt.ClickFocus)
-        MainWindow.setLayoutDirection(QtCore.Qt.LeftToRight)
+        MainWindow.resize(800, 591)
         MainWindow.setStyleSheet("background-color: rgb(0, 0, 0);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.playButton = QtWidgets.QPushButton(self.centralwidget)
-        self.playButton.setGeometry(QtCore.QRect(300, 220, 171, 41))
+        self.playButton.setGeometry(QtCore.QRect(300, 170, 171, 41))
         self.playButton.setStyleSheet("QPushButton{\n"
 "    background-Color: #3250a8;\n"
 "    border-radius: 10px;\n"
@@ -35,7 +33,7 @@ class Ui_MainWindow(object):
 "}")
         self.playButton.setObjectName("playButton")
         self.settingButton = QtWidgets.QPushButton(self.centralwidget)
-        self.settingButton.setGeometry(QtCore.QRect(300, 300, 171, 41))
+        self.settingButton.setGeometry(QtCore.QRect(300, 230, 171, 41))
         self.settingButton.setStyleSheet("QPushButton{\n"
 "    background-Color: #3250a8;\n"
 "    border-radius: 10px;\n"
@@ -48,8 +46,22 @@ class Ui_MainWindow(object):
 "    border-radius: 10px;\n"
 "}")
         self.settingButton.setObjectName("settingButton")
+        self.settingButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.settingButton_2.setGeometry(QtCore.QRect(300, 290, 171, 41))
+        self.settingButton_2.setStyleSheet("QPushButton{\n"
+"    background-Color: #3250a8;\n"
+"    border-radius: 10px;\n"
+"    font-size: 30px;\n"
+"    font: 25pt \"Georgia\";\n"
+"}\n"
+"\n"
+"QPushButton:Hover{\n"
+"    background-Color: #4b72e3;\n"
+"    border-radius: 10px;\n"
+"}")
+        self.settingButton_2.setObjectName("settingButton_2")
         self.title = QtWidgets.QLabel(self.centralwidget)
-        self.title.setGeometry(QtCore.QRect(230, 80, 331, 61))
+        self.title.setGeometry(QtCore.QRect(220, 60, 331, 61))
         self.title.setStyleSheet("font: 36pt \"Georgia\";\n"
 "color: rgb(255, 255, 255);")
         self.title.setObjectName("title")
@@ -68,9 +80,10 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.playButton.setText(_translate("MainWindow", "Play"))
-        self.settingButton.setText(_translate("MainWindow", "Settings"))
-        self.title.setText(_translate("MainWindow", "What the Git!"))
+        self.playButton.setText(_translate("MainWindow", "Level 1"))
+        self.settingButton.setText(_translate("MainWindow", "Level 2"))
+        self.settingButton_2.setText(_translate("MainWindow", "Level 3"))
+        self.title.setText(_translate("MainWindow", "Level Selector"))
 
 
 if __name__ == "__main__":

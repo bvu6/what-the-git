21def301e44cd049d3c8385d20d16fc402a18159
Vincent Tran <v_tran37@u.pacific.edu>
2022-02-17 04:11:51 -0800

    changed the card_select.py class to use qgraphictextitem instead of rect

--- pyqt5/card_select.py
+++ pyqt5/card_select.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # Made By Thicc-Juice
 import sys
-from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsRectItem
+from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsRectItem, QGraphicsTextItem
 from PyQt5.QtCore import Qt, QPointF
 
 
-class MovingCards(QGraphicsRectItem):
-    def __init__(self, x, y, r,l):
-        super().__init__(0, 0, r, l)
-        self.setPos(x, y)
-        self.setBrush(Qt.blue)
-        self.setAcceptHoverEvents(True)
+class MovingCards(QGraphicsTextItem):
+    # def __init__(self, x, y, r,l):
+    #     super().__init__(0, 0, r, l)
+    #     self.setPos(x, y)
+    #     self.setBrush(Qt.blue)
+    #     self.setAcceptHoverEvents(True)
 
     #mouse hover
     def hoverEnterEvent(self, event):
@@ -44,13 +44,27 @@ class GraphicView(QGraphicsView):
         self.setScene(self.scene)
         self.setSceneRect(0, 0, 1200, 1000)
 
-        self.moveObject = MovingCards(50, 50, 150, 200)
-        self.moveObject2 = MovingCards(250, 50, 150, 200)
-        self.moveObject3 = MovingCards(450, 50, 150, 200)
-        self.scene.addItem(self.moveObject)
-        self.scene.addItem(self.moveObject2)
-        self.scene.addItem(self.moveObject3)
-
+        # self.moveObject = MovingCards(50, 50, 150, 200)
+        # self.moveObject2 = MovingCards(250, 50, 150, 200)
+        # self.moveObject3 = MovingCards(450, 50, 150, 200)
+        # self.scene.addItem(self.moveObject)
+        # self.scene.addItem(self.moveObject2)
+        # self.scene.addItem(self.moveObject3)
+        self.test = MovingCards()
+        self.test.setPlainText("Git Add")
+        self.test.setScale(5)
+        self.test.setX(500)
+        self.scene.addItem(self.test)
+        self.test1 = MovingCards()
+        self.test1.setPlainText("Git Commit")
+        self.test1.setScale(5)
+        self.test.setX(200)
+        self.scene.addItem(self.test1)
+        self.test2 = MovingCards()
+        self.test2.setPlainText("Git Push")
+        self.test2.setScale(5)
+        self.test.setX(800)
+        self.scene.addItem(self.test2)
 
 app = QApplication(sys.argv)
 view = GraphicView()

ee384d76f583df464e9e601878fba331189d5ff6
Bikram Chatterjee <56491363+i0nics@users.noreply.github.com>
2022-02-23 19:56:07 -0800

    Enabled window switching between start and level

--- src/wtg_run.py
+++ src/wtg_run.py
@@ -3,8 +3,10 @@
 # What The Git!
 
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow
+
 from level_selector import LevelWindow
-from start_screen import Ui_MainWindow
+from start_screen import UI_start_window
 import sys
 
 
@@ -15,14 +17,27 @@ def switch_to_level_selector_window(self):
     self.window.show()
 
 
+class MainWindow:
+
+    def __init__(self):
+        self.main_window = QtWidgets.QMainWindow()
+        self.ui = UI_start_window(self.main_window)
+        self.stacked_widget = QtWidgets.QStackedWidget()
+        self.stacked_widget.addWidget(self.main_window)
+        self.stacked_widget.setFixedWidth(1280)
+        self.stacked_widget.setFixedHeight(720)
+        self.stacked_widget.show()
+        self.ui.playButton.clicked.connect(lambda: self.switch_to_level_selector())
+
+    def switch_to_level_selector(self):
+        self.stacked_widget.hide()
+        self.stacked_widget.removeWidget(self.main_window)
+        self.ui = LevelWindow(self.main_window)
+        self.stacked_widget.addWidget(self.main_window)
+        self.stacked_widget.show()
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    stacked_widget = QtWidgets.QStackedWidget()
-    MainWindow = QtWidgets.QMainWindow()
-
-    stacked_widget.add(start_s)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    main_window = MainWindow()
     sys.exit(app.exec_())
